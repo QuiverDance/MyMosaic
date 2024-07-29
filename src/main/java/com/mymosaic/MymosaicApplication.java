51a7b7f37c30fc7fb06a563dd2307983babbfb52
여름 <pjw7200@ajou.ac.kr>
2024-07-29 22:41:17 +0900

    Initial commit

---
+++ src/main/java/com/mymosaic/MymosaicApplication.java
@@ -0,0 +1,13 @@
+package com.mymosaic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymosaicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymosaicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mymosaic/MymosaicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymosaic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymosaicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
