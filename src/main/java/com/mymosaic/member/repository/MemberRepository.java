7505f70563fb731cb2c161c02e3fcddd6a4fa001
여름 <pjw7200@ajou.ac.kr>
2024-07-30 22:42:34 +0900

    add Member Repository

---
+++ src/main/java/com/mymosaic/member/repository/MemberRepository.java
@@ -0,0 +1,40 @@
+package com.mymosaic.member.repository;
+
+import com.mymosaic.member.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new ConcurrentHashMap<>();
+    private static Long sequence = 0L;
+
+    public void save(Member member){
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+    }
+
+    public Member findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Member> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    public Optional<Member> findByLoginId(String loginId){
+        return findAll().stream()
+                .filter(m -> m.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
