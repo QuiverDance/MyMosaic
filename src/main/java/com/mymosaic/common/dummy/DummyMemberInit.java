0df002220f4e8ba8fa56ebe77a8986b0671ff1f9
여름 <pjw7200@ajou.ac.kr>
2024-08-07 01:44:30 +0900

    feat : add new properties in member

--- src/main/java/com/mymosaic/common/dummy/DummyMemberInit.java
+++ src/main/java/com/mymosaic/common/dummy/DummyMemberInit.java
@@ -18,6 +18,13 @@ public class DummyMemberInit {
         Member member2 = new Member("test2", "테스터2", "123");
         Member member3 = new Member("test3", "테스터3", "123");
 
+        member1.updateProfileImgUrl("/members/profile/테스터1.png");
+        member2.updateProfileImgUrl("/members/profile/테스터2.png");
+        member3.updateProfileImgUrl("/members/profile/테스터3.png");
+        member1.updateIntroduction("테스터1 입니다.");
+        member2.updateIntroduction("테스터2 입니다.");
+        member3.updateIntroduction("테스터3 입니다.");
+
         memberRepository.save(member1);
         memberRepository.save(member2);
         memberRepository.save(member3);
