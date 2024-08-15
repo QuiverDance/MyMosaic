d8183f228d836a4e76fde0b45dd8850cfff8da7d
여름 <pjw7200@ajou.ac.kr>
2024-08-16 00:01:36 +0900

    feat : add introduction validation

--- src/main/java/com/mymosaic/member/web/MemberInfoEditForm.java
+++ src/main/java/com/mymosaic/member/web/MemberInfoEditForm.java
@@ -1,11 +1,13 @@
 package com.mymosaic.member.web;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class MemberInfoEditForm {
 
+    @Length(max = 5)
     private String introduction = "";
     private MultipartFile profileImg;
 }
