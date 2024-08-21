e97da5f3356cd3937512367ad3a528ec8553dc8c
여름 <pjw7200@ajou.ac.kr>
2024-08-22 01:09:28 +0900

    feat : DiaryRepository is registered for bean

--- src/main/java/com/mymosaic/RepositoryConfig.java
+++ src/main/java/com/mymosaic/RepositoryConfig.java
@@ -1,5 +1,7 @@
 package com.mymosaic;
 
+import com.mymosaic.diary.repository.DiaryRepository;
+import com.mymosaic.diary.repository.MemoryDiaryRepository;
 import com.mymosaic.member.repository.MemberRepository;
 import com.mymosaic.member.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
@@ -11,5 +13,10 @@ public class RepositoryConfig {
     @Bean
     public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public DiaryRepository diaryRepository(){
+        return new MemoryDiaryRepository();
     }
 }
