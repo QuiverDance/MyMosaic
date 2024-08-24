6f96ae0f3dd32f2b10a49e9ec7805a0f6d5a5850
여름 <pjw7200@ajou.ac.kr>
2024-08-24 14:12:44 +0900

    feat : diaryDto

---
+++ src/main/java/com/mymosaic/diary/dto/DiaryDto.java
@@ -0,0 +1,39 @@
+package com.mymosaic.diary.dto;
+
+import com.mymosaic.common.file.UploadFile;
+import com.mymosaic.diary.domain.Diary;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class DiaryDto {
+
+    private Long id;
+    private Long memberId;
+    private Boolean isPublic;
+    private Boolean bookmark;
+    private String title;
+    private String content;
+    private LocalDate dairyDate;
+    private LocalDateTime uploadTime;
+    private LocalDateTime lastTime;
+
+    private List<UploadFile> files;
+    private List<String> encodedFiles;
+
+    public DiaryDto(Diary diary){
+        id = diary.getId();
+        memberId = diary.getMemberId();
+        isPublic = diary.getIsPublic();
+        bookmark = diary.getBookmark();
+        title = diary.getTitle();
+        content = diary.getContent();
+        dairyDate = diary.getDairyDate();
+        uploadTime = diary.getUploadTime();
+        lastTime = diary.getLastTime();
+        files = diary.getFiles();
+    }
+}
