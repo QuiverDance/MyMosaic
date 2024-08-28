a86e06b3532f7861823452f5389fb9243525b65f
여름 <pjw7200@ajou.ac.kr>
2024-08-29 00:56:38 +0900

    refactor : add noArgsConstructor and default values

--- src/main/java/com/mymosaic/diary/dto/DiarySearchAndSortParam.java
+++ src/main/java/com/mymosaic/diary/dto/DiarySearchAndSortParam.java
@@ -2,14 +2,16 @@ package com.mymosaic.diary.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class DiarySearchAndSortParam {
 
     String keyword;
     Integer year;
     Integer month;
-    String sortBy;
-    String sortDir;
+    String sortBy = "date";
+    String sortDir = "asc";
 }
