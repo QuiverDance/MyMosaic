417ffc0bb44b6f181912e51d500f60b94ec73560
여름 <pjw7200@ajou.ac.kr>
2024-09-08 23:49:53 +0900

    refactor : merge subClass

--- src/main/java/com/mymosaic/hall/web/WorkAddForm.java
+++ src/main/java/com/mymosaic/hall/web/WorkAddForm.java
@@ -1,13 +1,23 @@
 package com.mymosaic.hall.web;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Data
-public abstract class WorkAddForm {
+public class WorkAddForm {
 
     private Integer categoryId;
     private Boolean visibility;
     private String name;
     private String content;
     private Float rating;
+
+    private Integer subCategoryId;
+    private List<Integer> genreIds;
+    private String production;
+    private List<String> performers;
+    private Integer year;
+    private List<MultipartFile> workImageFiles;
 }
